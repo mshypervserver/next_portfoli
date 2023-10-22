db1a00d633b004afd368efe92feae44ab6c2d276
mshypervserver <148624225+mshypervserver@users.noreply.github.com>
2023-10-23 11:03:45 +1300

    Update TabButton.jsx

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 function TabButton({active, selectTab, children }) {
@@ -14,21 +13,5 @@ function TabButton({active, selectTab, children }) {
     </button>)
 }
 
-=======
-import React from 'react'
 
-function TabButton({active, selectTab, children }) {
-    const buttonClasses = active ? 'text-[#ADB7BE] border-b border-purple-500' : 'text-white'
-
-  return (
-    <button onClick={selectTab}>
-        <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
-        {children}
-        </p>
- 
- 
-    </button>)
-}
-
->>>>>>> 378cc9e485104b3e6d2f04a9c94101fadd100d80
 export default TabButton
