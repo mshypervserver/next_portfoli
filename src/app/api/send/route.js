efd1fc1b6d41c33cb0bace32b39cc896e7ba975d
mshypervserver <148624225+mshypervserver@users.noreply.github.com>
2023-10-23 11:04:13 +1300

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
@@ -22,28 +21,4 @@ export async function POST() {
     return NextResponse.json({ error });
   }
 }
-=======
-//import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'VM <mshypervserver@gmail.com>',
-      to: ['mshypervserver@gmail.com'],
-      subject: 'Hello world',
-      react: <>
-        <h1>Hello world</h1>
-        <p>Email body</p>
-      </>,
-    });
-
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
->>>>>>> 378cc9e485104b3e6d2f04a9c94101fadd100d80
