4bcb87b09c547c83668eec51db391d2c03427d52
mshypervserver <148624225+mshypervserver@users.noreply.github.com>
2023-10-23 11:03:35 +1300

    Update Navlink.jsx

--- src/app/components/Navlink.jsx
+++ src/app/components/Navlink.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Link from 'next/link'
 
@@ -14,23 +13,4 @@ import Link from 'next/link'
   )
 }
 
-
-=======
-import React from 'react'
-import Link from 'next/link'
-
- const Navlink = ({ href, title }) => {
-
-  return (
-    <Link
-        href={href}
-        className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-  >
-    {title}
-  </Link>
-  )
-}
-
-
->>>>>>> 378cc9e485104b3e6d2f04a9c94101fadd100d80
 export default Navlink;
