8c738817628e875e715c4a6433105a2c32bf2e33
mshypervserver <148624225+mshypervserver@users.noreply.github.com>
2023-10-23 11:03:12 +1300

    Update Menu.jsx

--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import Navlink from './Navlink'
 
@@ -12,19 +12,4 @@ const Menu =({ links }) => {
   )
 }
 
-=======
-import React from 'react'
-import Navlink from './Navlink'
-
-const Menu =({ links }) => {
-  return (
-    <ul className='flex flex-col py-4 items-center'>
-        {links.map((link, index) => 
-        (<li key={index}>
-            <Navlink href={link.path} title={link.title}/>
-        </li>))}</ul>
-  )
-}
-
->>>>>>> 378cc9e485104b3e6d2f04a9c94101fadd100d80
 export default Menu
